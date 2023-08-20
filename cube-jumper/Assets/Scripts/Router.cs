0c05dc81e4cef173b21e864db00ef669f643bf4d
Berkay <berkaykiziltug98@gmail.com>
2023-08-20 17:43:24 +0300

    Route trigger is set!

--- cube-jumper/Assets/Scripts/Router.cs
+++ cube-jumper/Assets/Scripts/Router.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Router : MonoBehaviour
 {
+    PlayerController playerController;
+    Rigidbody playerRb;
+    public float moveSpeed = -90;
+    public float burstX = -1;
+    Vector3 counterMoveDirection = new Vector3 (0,0,1);
+    Vector3 boostX = new Vector3(.5f,0,0);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        playerRb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.tag== "Player")
+        {   
+            playerRb.AddForce(boostX*burstX,ForceMode.Impulse);
+            playerRb.AddForce(counterMoveDirection*moveSpeed,ForceMode.Impulse);
+            playerController.MoveLeftward();
+        }
     }
 }
