3db04ca195823a425eef8090ebd97cb2e833675b
Berkay <berkaykiziltug98@gmail.com>
2023-10-02 19:48:40 +0300

    Gameplay mechanics and only the play button
    Added ground check. Set up win lose conditions. Set up ReloadManager. Set up the functionality of the play button. Also fixed the prefabs and properly labeled them all.

---
+++ cube-jumper/Assets/Scripts/PlayerPhysicsController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPhysicsController : MonoBehaviour
+{
+    Rigidbody _rb;
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] LayerMask _groundLayer;
+    [SerializeField] Transform _groundCheck;
+    private bool canJump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            canJump = true;
+        }
+       
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        if (canJump)
+        {
+            Jump();
+            //set it to false after jump because we dont wanna go flying upwards.
+            canJump = false;
+        }
+    }
+
+    private void Jump()
+    {   
+        {
+            _rb.AddForce(transform.up * _jumpForce, ForceMode.Impulse);
+        }
+
+    }
+
+    private void Move()
+    {
+        Vector3 forward = transform.forward;
+        float currentYVel = _rb.velocity.y;
+        Vector3 newVelocity = transform.forward * _moveSpeed;
+        newVelocity.y = currentYVel;
+        _rb.velocity = newVelocity;
+        //_rb.velocity = transform.forward * _moveSpeed;
+    }
+    private bool IsGrounded()
+    {
+       
+        return Physics.CheckSphere(_groundCheck.position, .002f, _groundLayer);
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(_groundCheck.position, .002f);
+    }
+    
+
+}
