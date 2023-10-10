15e0ae115b3aaa7f0ab56f37cb2ad56a11038137
Berkay <berkaykiziltug98@gmail.com>
2023-10-10 17:04:51 +0300

    Game Over Screen, Route coroutine.
    Made game over screen. Assigned the button functionalities. Disabled triggering router for more than once in the same frame.

--- cube-jumper/Assets/Scripts/WinLoseCondition.cs
+++ cube-jumper/Assets/Scripts/WinLoseCondition.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class WinLoseCondition : MonoBehaviour
-{ 
-
+{
+    public GameOverScreen gameOver;
     private bool gameEnded;
+
     public void WinLevel()
     {
         if (!gameEnded)
         {
             Debug.Log("You Win!");
+            gameOver.GameOver();
             gameEnded = true;
         }
     }
@@ -21,12 +24,8 @@ public class WinLoseCondition : MonoBehaviour
         if (!gameEnded)
         {
             Debug.Log("You Lose..");
+            gameOver.GameOver();
             gameEnded = true;
         }
-    }
-
-    public void ReloadManager(int sceneNumber)
-    {
-        SceneManager.LoadScene(sceneNumber);
     }
 }
