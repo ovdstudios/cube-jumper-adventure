ac03c6d15ee244c55c5c369d854d10f97ccdce34
Berkay <berkaykiziltug98@gmail.com>
2023-10-24 16:28:45 +0300

    Latest assets imported from blender and other changes
    Space to start screen. Assets are now imported again from blender with applied transforms. Added a new movement whenever the player is not on the ground, we add force while in the air so that we get the "leap" feeling.

--- cube-jumper/Assets/Scripts/GameOverScreen.cs
+++ cube-jumper/Assets/Scripts/GameOverScreen.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,13 +7,13 @@ public class GameOverScreen : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void GameOver()
     {
--- cube-jumper/Assets/Scripts/PlayerPhysicsController.cs
+++ cube-jumper/Assets/Scripts/PlayerPhysicsController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerPhysicsController : MonoBehaviour
@@ -11,6 +9,8 @@ public class PlayerPhysicsController : MonoBehaviour
     [SerializeField] Transform _groundCheck;
     [SerializeField] ParticleSystem _jumpParticle;
     [SerializeField] AudioSource _jumpSound;
+    [SerializeField] private float _gravity = -5;
+    [SerializeField] private float airMoveSpeed = 10;
     private bool canJump;
     PlayerTriggers playerTrigger;
 
@@ -18,6 +18,7 @@ public class PlayerPhysicsController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         _rb = GetComponent<Rigidbody>();
         playerTrigger = GetComponent<PlayerTriggers>();
     }
@@ -25,12 +26,12 @@ public class PlayerPhysicsController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     
+
         if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
             canJump = true;
         }
-       
+
     }
 
     private void FixedUpdate()
@@ -46,6 +47,17 @@ public class PlayerPhysicsController : MonoBehaviour
                 //set it to false after jump because we dont wanna go flying upwards.
                 canJump = false;
             }
+
+            if (IsGrounded())
+            {
+                Move();
+            }
+            else if (!IsGrounded())
+            {
+                //belki force mode impulse deneyebiliriz.
+                Leap();
+            }
+
         }
         else
         {
@@ -53,14 +65,17 @@ public class PlayerPhysicsController : MonoBehaviour
             _rb.velocity = Vector3.zero;
         }
 
+
+
+
     }
 
     private void Jump()
-    {   
+    {
         {
             _jumpSound.Play();
             CreateJumpParticle();
-            _rb.AddForce(transform.up * _jumpForce, ForceMode.Impulse);
+            _rb.AddForce(transform.up * _jumpForce * _gravity, ForceMode.Impulse);
         }
 
     }
@@ -74,18 +89,23 @@ public class PlayerPhysicsController : MonoBehaviour
         _rb.velocity = newVelocity;
         //_rb.velocity = transform.forward * _moveSpeed;
     }
+
+    private void Leap()
+    {
+        _rb.AddForce(transform.forward * airMoveSpeed, ForceMode.Force);
+    }
     private bool IsGrounded()
     {
-       
+
         return Physics.CheckSphere(_groundCheck.position, .002f, _groundLayer);
-        
+
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawSphere(_groundCheck.position, .002f);
     }
-    
+
     private void CreateJumpParticle()
     {
         _jumpParticle.Play();
